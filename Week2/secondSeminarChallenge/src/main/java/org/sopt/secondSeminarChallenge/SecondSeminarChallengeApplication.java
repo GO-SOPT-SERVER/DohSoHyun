6ec6acd2e3250d7e6096b08920e56614403d0c91
sohyundoh <lyny.doh@gmail.com>
2023-04-10 22:36:01 +0900

    #7 2차시 도전 과제 :initializing

---
+++ Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/SecondSeminarChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.secondSeminarChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSeminarChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSeminarChallengeApplication.class, args);
+	}
+
+}
