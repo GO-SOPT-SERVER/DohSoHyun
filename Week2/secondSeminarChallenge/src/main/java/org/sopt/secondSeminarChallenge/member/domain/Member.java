9402cd2c5fdd72842fb95f9348ba039f03bdff2e
sohyundoh <lyny.doh@gmail.com>
2023-04-10 22:51:39 +0900

    #7 2차시 도전 과제: Member Domain 작성
    getter 어노테이션 사용

--- Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/member/domain/Member.java
+++ Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/member/domain/Member.java
@@ -1,2 +1,27 @@
-package org.sopt.secondSeminarChallenge.member.domain;public class Member {
+package org.sopt.secondSeminarChallenge.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Member {
+    private Long id;
+    private String email;
+    private String name;
+
+    public Member(Long id, String email, String name){
+        this.email = email;
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    @Override
+    public String toString(){
+        return "id: " + this.id + "\n" +
+                "email: "+ this.email + "\n" +
+                "name" + this.name + "\n";
+    }
 }
