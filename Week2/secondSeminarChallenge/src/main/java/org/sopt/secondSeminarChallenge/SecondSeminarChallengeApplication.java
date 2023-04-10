32f02b690227df43f8ad56002135d859331549ba
sohyundoh <lyny.doh@gmail.com>
2023-04-10 22:54:24 +0900

    #7 2차시 도전 과제: ArrayList로 가상 DB 만들기

--- Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/SecondSeminarChallengeApplication.java
+++ Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/SecondSeminarChallengeApplication.java
@@ -1,13 +1,18 @@
 package org.sopt.secondSeminarChallenge;
 
+import org.sopt.secondSeminarChallenge.member.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecondSeminarChallengeApplication {
 
+	public static ArrayList<Member> memberList;
 	public static void main(String[] args) {
 		SpringApplication.run(SecondSeminarChallengeApplication.class, args);
+		memberList = new ArrayList<>();
 	}
 
 }
