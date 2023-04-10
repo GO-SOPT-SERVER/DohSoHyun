5841be0bfd81116e465379e7e53768bed5b56642
sohyundoh <lyny.doh@gmail.com>
2023-04-10 22:52:09 +0900

    #7 2차시 도전 과제: MemberSaveRequestDto 작성

---
+++ Week2/secondSeminarChallenge/src/main/java/org/sopt/secondSeminarChallenge/member/dto/MemberSaveRequestDto.java
@@ -0,0 +1,13 @@
+package org.sopt.secondSeminarChallenge.member.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberSaveRequestDto {
+    private Long id;
+    private String email;
+    private String name;
+}
