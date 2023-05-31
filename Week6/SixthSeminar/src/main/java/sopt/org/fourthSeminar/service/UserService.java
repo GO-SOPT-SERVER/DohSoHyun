afb715f3e1b2e9e11835cb0a1b9b3e8006e9a819
sohyundoh <lyny.doh@gmail.com>
2023-05-31 21:36:13 +0900

    #14 6차시 심화 과제 : Service

--- Week6/SixthSeminar/src/main/java/sopt/org/fourthSeminar/service/UserService.java
+++ Week6/SixthSeminar/src/main/java/sopt/org/fourthSeminar/service/UserService.java
@@ -3,14 +3,18 @@ package sopt.org.fourthSeminar.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sopt.org.fourthSeminar.config.jwt.JwtService;
+import sopt.org.fourthSeminar.controller.dto.request.AccessTokenRequestDto;
 import sopt.org.fourthSeminar.controller.dto.request.UserLoginRequestDto;
 import sopt.org.fourthSeminar.controller.dto.request.UserRequestDto;
+import sopt.org.fourthSeminar.controller.dto.response.NewAccessTokenResponseDto;
 import sopt.org.fourthSeminar.controller.dto.response.UserResponseDto;
 import sopt.org.fourthSeminar.domain.User;
 import sopt.org.fourthSeminar.exception.Error;
 import sopt.org.fourthSeminar.exception.model.BadRequestException;
 import sopt.org.fourthSeminar.exception.model.ConflictException;
 import sopt.org.fourthSeminar.exception.model.NotFoundException;
+import sopt.org.fourthSeminar.exception.model.UnauthorizedException;
 import sopt.org.fourthSeminar.infrastructure.UserRepository;
 
 @Service
@@ -18,6 +22,7 @@ import sopt.org.fourthSeminar.infrastructure.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     @Transactional
     public UserResponseDto create(UserRequestDto request) {
@@ -45,5 +50,15 @@ public class UserService {
         }
 
         return user.getId();
+    }
+
+    @Transactional
+    public NewAccessTokenResponseDto auto_login(AccessTokenRequestDto requestDto) throws UnauthorizedException{
+        if(!jwtService.verifyToken(requestDto.getRefreshToken())){
+            throw new UnauthorizedException(Error.TOKEN_NOT_VERIFIED_EXCEPTION, Error.TOKEN_NOT_VERIFIED_EXCEPTION.getMessage());
+        }
+        Long userId = Long.valueOf(jwtService.getJwtContents(requestDto.getRefreshToken()));
+        String newAccessKey = jwtService.issuedAccessToken(String.valueOf(userId));
+        return NewAccessTokenResponseDto.of(userId, newAccessKey);
     }
 }
